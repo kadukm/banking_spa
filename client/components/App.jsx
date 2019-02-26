b383fea62f942954b4690d77dcca5436a0497a27
kadukm <mk.shark25@gmail.com>
2019-02-26 15:21:50 +0500

    add styles

---
+++ client/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/App.css'
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div className='test'>Eeeee Reaaact!!!</div>
+        );
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './index.jsx',
+  entry: './client/index.jsx',
   devtool: "source-map",
   output: {
     filename: 'bundle.js',
@@ -14,6 +14,10 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: ["babel-loader"]
+      },
+      {
+        test: /\.css$/,
+        loader: ['style-loader', 'css-loader']
       }
     ]
   }
