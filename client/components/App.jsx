a49aef0c3efbd32188dcca2ab85ba2978e1f9c11
kadukm <mk.shark25@gmail.com>
2019-02-26 22:35:18 +0500

    add simple routing

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import '../styles/App.css'
 
 export default class App extends React.Component {
     render() {
         return (
-            <div className='test'>Eeeee Reaaact!!!</div>
+            <BrowserRouter>
+                <Switch>
+                    <Route
+                        path="/admin-panel"
+                        component={() => <div>Hello, Admin!</div>} />
+                    <Route
+                        path="/companies/:companyId"
+                        component={({ match }) => <div>Info about {match.params.companyId}!</div>}
+                    />
+                    <Route
+                        component={() => <div>Default</div>}
+                    />
+                </Switch>
+            </BrowserRouter>
         );
     }
 }
--- main.go
+++ main.go
@@ -4,7 +4,16 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
+
 	r.Static("/dist", "./dist")
+	r.StaticFile("/admin-panel", "./index.html")
 	r.StaticFile("/", "./index.html")
+	r.GET("/companies/:companyId", indexHandler)
+	r.HEAD("/companies/:companyId", indexHandler)
+
 	r.Run()
+}
+
+func indexHandler(c *gin.Context) {
+	c.File("./index.html")
 }
