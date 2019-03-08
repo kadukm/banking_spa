abb3b964e29eb443a7c54e864b897ed632c00809
kadukm <mk.shark25@gmail.com>
2019-03-08 18:27:48 +0500

    move getting info about company to Company-component (via Promises)

--- client/components/company/Company.jsx
+++ client/components/company/Company.jsx
@@ -3,13 +3,36 @@ import Header from './parts/Header.jsx'
 import Body from './parts/Body.jsx'
 import Footer from './parts/Footer.jsx'
 import Delimiter from '../utils/Delimiter.jsx'
+import apiBaseUrl from '../../config.js' 
 
+function getDataFrom(url) {
+    return new Promise(
+        resolve => fetch(url)
+            .then(response => response.json())
+            .then(result => resolve(result))
+    )
+}
 
 export default class Company extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {ok: undefined, companyInfo: undefined}
+    }
+
+    componentDidMount() {
+        const companyID = this.props.match.params.companyID
+        Promise.all([getDataFrom(`${apiBaseUrl}/api/companies/${companyID}`)])
+            .then(([companyInfo]) => this.setState({ok: true, companyInfo: companyInfo}))
+    }
+
     render() {
+        if (this.state.ok === undefined)
+            return <div>Loading...</div>
+        if (this.state.ok === false)
+            return <div>Can't load data about {this.props.match.params.companyID}</div>
         return (
             <div>
-                <Header companyID={this.props.match.params.companyID}/>
+                <Header {...this.state.companyInfo}/>
                 <Delimiter />
                 <Body />
                 <Delimiter />
--- client/components/company/parts/Header.jsx
+++ client/components/company/parts/Header.jsx
@@ -1,38 +1,18 @@
 import React from 'react';
-import apiBaseUrl from '../../../config.js' 
 
 export default class Header extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {info: undefined}
-    }
-
-    componentDidMount() {
-        fetch(`${apiBaseUrl}/api/companies/${this.props.companyID}`)
-            .then(response => response.json())
-            .then(res => this.setState({info: res}))
-    }
-
     render() {
         return (
-            this.state.info !== undefined
-                ? this.renderHeader()
-                : null
-        )
-    }
-
-    renderHeader = () => {
-        return (
             <section>
-                <img src={this.state.info.photo_path} alt="user photo" />
-                <h3>{this.state.info.status} {this.state.info.name}</h3>
+                <img src={this.props.photo_path} alt="user photo" />
+                <h3>{this.props.status} {this.props.name}</h3>
                 <div>
-                    <span>{this.state.info.phone} </span>
-                    <a href={"http://" + this.state.info.site}>{this.state.info.site}</a>
-                    <a href={"mailto:" + this.state.info.email}>{this.state.info.email}</a>
+                    <span>{this.props.phone} </span>
+                    <a href={"http://" + this.props.site}>{this.props.site}</a>
+                    <a href={"mailto:" + this.props.email}>{this.props.email}</a>
                 </div>
-                <a href={this.state.info.full_info_path}>Информация о компании</a>
-                <a href={this.state.info.requisites_path}>Показать реквизиты</a>
+                <a href={this.props.full_info_path}>Информация о компании</a>
+                <a href={this.props.requisites_path}>Показать реквизиты</a>
             </section>
         )
     }
