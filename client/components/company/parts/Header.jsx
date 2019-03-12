da4bd0089e1787e3f06970d12d411291125f566f
kadukm <mk.shark25@gmail.com>
2019-03-12 21:11:27 +0500

    add simple styles for Header

--- client/components/company/parts/Header.jsx
+++ client/components/company/parts/Header.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import "../../../styles/Header.css"
 
 export default class Header extends React.Component {
     render() {
         return (
-            <section>
-                <img src={this.props.photo_path} alt="user photo" />
-                <h3>{this.props.status} {this.props.name}</h3>
-                <div>
-                    <span>{this.props.phone} </span>
-                    <a href={"http://" + this.props.site}>{this.props.site}</a>
+            <section className="user-info box-wrapper">
+                <img className="user-info__photo" src={this.props.photo_path} alt="user" />
+                <h3 className="user-info__header">{this.props.status} {this.props.name}</h3>
+                <div className="user-info__contacts">
+                    <span className="user-info__phone">{this.props.phone} </span>
+                    <a className="user-info__site" href={"http://" + this.props.site}>{this.props.site}</a>
                     <a href={"mailto:" + this.props.email}>{this.props.email}</a>
                 </div>
-                <a href={this.props.full_info_path}>Информация о компании</a>
-                <a href={this.props.requisites_path}>Показать реквизиты</a>
+                <a className="user-info__company" href={this.props.full_info_path}>Информация о компании</a>
+                <a className="user-info__requisites" href={this.props.requisites_path}>Показать реквизиты</a>
             </section>
         )
     }
