4645b504d53e8cadc8f15c6c17345e260c57c827
kadukm <mk.shark25@gmail.com>
2019-03-15 20:04:50 +0500

    move styles for wrong input to Body.css

--- client/components/company/parts/Body.jsx
+++ client/components/company/parts/Body.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Request from '../payments/Request.jsx'
 import Payment from '../payments/Payment.jsx'
+import '../../../styles/Body.css'
 
 
 export default class Body extends React.Component {
@@ -13,13 +14,13 @@ export default class Body extends React.Component {
 
     render() {
         return (
-            <div>
+            <section>
                 <button onClick={() => this.setState({isRequest: false})}>Заплатить</button>
                 <button onClick={() => this.setState({isRequest: true})}>Запросить платёж</button>
                 {this.state.isRequest
                     ? <Request status={this.props.status} name={this.props.name}/>
                     : <Payment />}
-            </div>
+            </section>
         )
     }
 }
--- client/components/company/payments/Request.jsx
+++ client/components/company/payments/Request.jsx
@@ -55,7 +55,7 @@ export default class Request extends React.Component {
                 <div>
                     <div>
                         <label htmlFor="inn">ИНН получателя</label>
-                        <input className={this.state.ok.inn === false ? 'wrong-phone' : ''}
+                        <input className={this.state.ok.inn === false ? 'wrong-input' : ''}
                             type="text"
                             name="inn"
                             id="inn"
@@ -67,7 +67,7 @@ export default class Request extends React.Component {
                     </div>
                     <div>
                         <label htmlFor="bik">БИК</label>
-                        <input className={this.state.ok.bik === false ? 'wrong-phone' : ''}
+                        <input className={this.state.ok.bik === false ? 'wrong-input' : ''}
                             type="text"
                             name="bik"
                             id="bik"
@@ -79,7 +79,7 @@ export default class Request extends React.Component {
                     </div>
                     <div>
                         <label htmlFor="account_number">Номер счёта</label>
-                        <input className={this.state.ok.account_number === false ? 'wrong-phone' : ''}
+                        <input className={this.state.ok.account_number === false ? 'wrong-input' : ''}
                             type="text"
                             name="account_number"
                             id="account_number"
@@ -91,7 +91,7 @@ export default class Request extends React.Component {
                     </div>
                     <div>
                         <label htmlFor="for_what">За что</label>
-                        <input className={this.state.ok.for_what === false ? 'wrong-phone' : ''}
+                        <input className={this.state.ok.for_what === false ? 'wrong-input' : ''}
                             type="text"
                             id="for_what"
                             name="for_what"
@@ -102,7 +102,7 @@ export default class Request extends React.Component {
                     </div>
                     <div>
                         <label htmlFor="amount">Сколько</label>
-                        <input className={this.state.ok.amount === false ? 'wrong-phone' : ''}
+                        <input className={this.state.ok.amount === false ? 'wrong-input' : ''}
                             type="text"
                             name="amount"
                             id="amount"
@@ -114,7 +114,7 @@ export default class Request extends React.Component {
                     </div>
                     <div>
                         <label htmlFor="phone">Телефон</label>
-                        <input className={this.state.ok.phone === false ? 'wrong-phone' : ''}
+                        <input className={this.state.ok.phone === false ? 'wrong-input' : ''}
                             type="tel"
                             name="phone"
                             id="phone"
@@ -126,7 +126,7 @@ export default class Request extends React.Component {
                     </div>
                     <div>
                         <label htmlFor="email">Ваш email</label>
-                        <input className={this.state.ok.email === false ? 'wrong-phone' : ''}
+                        <input className={this.state.ok.email === false ? 'wrong-input' : ''}
                             type="email"
                             name="email"
                             id="email"
