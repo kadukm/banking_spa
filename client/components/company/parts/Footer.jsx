59bf2a5e6f9aef4361f27e1b09a7b7b42c04c040
kadukm <mk.shark25@gmail.com>
2019-03-16 00:58:51 +0500

    add more styles

--- client/components/company/parts/Footer.jsx
+++ client/components/company/parts/Footer.jsx
@@ -4,12 +4,14 @@ import "../../../styles/Footer.css"
 export default class Footer extends React.Component {
     render() {
         return (
-            <section className="box-wrapper">
+            <section>
                 <h3>О компании {this.props.status} {this.props.name}</h3>
                 <div className="products">
                     {this.props.products.map(product => <Product {...product} key={product.name}/>)}
                 </div>
-                <div>{this.props.info}</div>
+                <div className="products__info">
+                    {this.props.info}
+                </div>
                 <a href={this.props.full_info_path}>Полная информация</a>
             </section>
         )
