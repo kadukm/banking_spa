0413c22b413683b0639096f170f1888627d183eb
kadukm <mk.shark25@gmail.com>
2019-03-19 19:49:17 +0500

    write simple architecture for admin-panel

--- client/components/admin-panel/AdminPanel.jsx
+++ client/components/admin-panel/AdminPanel.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import Requests from './Requests.jsx'
+import PaymentsFromCard from './PaymentsFromCard.jsx'
 
 export default class AdminPanel extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {isRequests: false}
+    }
     render() {
         return (
-            <div>Admin-panel</div>
+            <div>
+                <button onClick={() => this.setState({isRequests: false})}>
+                    Платежи с карты
+                </button>
+                <button onClick={() => this.setState({isRequests: true})}>
+                    Запрошенные платежи
+                </button>
+                {this.state.isRequests
+                    ? <Requests />
+                    : <PaymentsFromCard />}
+            </div>
         )
     }
 }
---
+++ client/components/admin-panel/PaymentsFromCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class PaymentsFromCard extends React.Component {
+    render() {
+        return (
+            <div>Payments from card</div>
+        )
+    }
+}
