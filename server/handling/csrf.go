e539bd539c41096987ce7ab819f19479a0fcdc1b
kadukm <mk.shark25@gmail.com>
2019-03-28 12:50:36 +0500

    move csrf.go into handling package

--- server/handling/csrf.go
+++ server/handling/csrf.go
@@ -1,16 +1,17 @@
-package utils
+package handling
 
 import (
 	"math/rand"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/kadukm/banking_spa/server/utils"
 )
 
 const (
-	CSRFTokenName  = "X-CSRF-TOKEN"
-	tokenLength    = 64
-	allowedLetters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM_1234567890"
+	CSRFTokenName      = "X-CSRF-TOKEN"
+	tokenLength        = 64
+	allowedCSRFLetters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM_1234567890"
 )
 
 func CSRFGeneration(c *gin.Context) {
@@ -22,16 +23,16 @@ func CheckCSRFToken(c *gin.Context) {
 	tokenFromHeader := c.GetHeader(CSRFTokenName)
 	tokenFromCookie, err := c.Cookie(CSRFTokenName)
 	if err != nil {
-		c.AbortWithStatusJSON(http.StatusBadRequest, ServerResponse{Ok: false, Result: err.Error()})
+		c.AbortWithStatusJSON(http.StatusBadRequest, utils.ServerResponse{Ok: false, Result: err.Error()})
 	} else if tokenFromHeader != tokenFromCookie {
-		c.AbortWithStatusJSON(http.StatusBadRequest, ServerResponse{Ok: false, Result: "Wrong CSRF Token"})
+		c.AbortWithStatusJSON(http.StatusBadRequest, utils.ServerResponse{Ok: false, Result: "Wrong CSRF Token"})
 	}
 }
 
 func generateCSRFToken() string {
 	res := make([]byte, tokenLength)
 	for i := range res {
-		res[i] = allowedLetters[rand.Intn(len(allowedLetters))]
+		res[i] = allowedCSRFLetters[rand.Intn(len(allowedCSRFLetters))]
 	}
 	return string(res)
 }
